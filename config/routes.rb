626e4485a5662185ba8ace5f681a5f6e5c6f2b2a
YudaiInoue <139064232+YudaiInoue@users.noreply.github.com>
2023-07-23 14:37:34 +0000

    [Finish] 4-19

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ root to: "homes#top"
 get '/about' => 'homes#about'
 
 resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+  resource :favorites, only: [:create, :destroy]
   resources :post_comments, only: [:create, :destroy]
 end
 resources :users, only: [:show, :edit, :update]
